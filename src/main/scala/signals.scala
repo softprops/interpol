a63d3b4ef338c7a0dc8b22e62f1dab12b2631391
softprops <d.tangren@gmail.com>
2013-08-24 01:20:35 -0400

    restructuring

--- src/main/scala/signals.scala
+++ src/main/scala/signals.scala
@@ -6,37 +6,40 @@ import scala.util.control.Exception.catching
 object Signals {
   def apply() = {
     catching(classOf[ClassNotFoundException]).opt {
-      Class.forName("sun.misc.Signal")
-      new Signals()
+      new Signals(Class.forName("sun.misc.SignalHandler"), Class.forName("sun.misc.SignalHandler"))
     }
   }
 }
 
-private [interpol] class Signals {
-  private val handlerCls = Class.forName("sun.misc.SignalHandler")
-  private val signalCls = Class.forName("sun.misc.Signal")
-  private val handleMeth = signalCls.getMethod("handle", signalCls, handlerCls)
-  private val nameMeth = signalCls.getMethod("getName")
-
-  private def signalName(arg: Object) =
+private [interpol] class Signaler(cls: Class[_], handlerCls: Class[_]) {
+  private val handleMeth = cls.getMethod("handle", cls, handlerCls)
+  private val nameMeth   = cls.getMethod("getName")
+  val handle = handleMeth
+  val name = nameMeth.getName
+  def name(arg: Object) =
     nameMeth.invoke(arg).asInstanceOf[String]
-  private def newInstance(signal: String) =
-    signalCls.getConstructor(classOf[String])
-             .newInstance(signal)
-             .asInstanceOf[Object]
-  def apply(signal: String, handler: String => Unit) {
+  def instance(signal: String) =
+    cls.getConstructor(classOf[String])
+       .newInstance(signal)
+       .asInstanceOf[Object]
+}
+
+private [interpol] class Signals(
+  handlerCls: Class[_], signalCls: Class[_]) {
+  val sig = new Signaler(signalCls, handlerCls)
+  def apply(signal: String)(handler: String => Unit) {
     val prox = Proxy.newProxyInstance(
       handlerCls.getClassLoader,
       Array[Class[_]](handlerCls),
       new InvocationHandler {
         def invoke(proxy: Object, method: Method, args: Array[Object]) = {
-          if (method.getName == handleMeth.getName) {
-            handler(signalName(args(0)))
+          if (method.getName == sig.name) {
+            handler(sig.name(args(0)))
           }
           null
         }
       })
-    handleMeth.invoke(null, newInstance(signal), prox)
+    sig.handle.invoke(null, sig.instance(signal), prox)
   }
 }
 
